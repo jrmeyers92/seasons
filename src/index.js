fa517d00af1a517ed03e047335258bc922774fd9
Jake Meyers <JakeRMeyers92@gmail.com>
2020-09-11 17:06:32 -0600

    app almost done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./spinner";
 
 class App extends React.Component {
   state = {lat: null, errorMessage: ""}
@@ -22,8 +23,12 @@ class App extends React.Component {
       return <SeasonDisplay lat={this.state.lat} />
     }
 
-    return <div>Loading!</div>;
-  }
-}
+    return (
+        <div>
+            <Spinner text= "please accept geolocation."/>
+        </div>
+    );
+  };
+};
 
 ReactDOM.render(<App />, document.querySelector('#root'));
